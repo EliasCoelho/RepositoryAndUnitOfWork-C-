7eb6ca1807c8f4d795fe0e6e8e6770511da8e3ac
Elias Coelho <elias_coelho19@hotmail.com>
2016-09-09 01:54:05 -0400

    Repository and Unit of Work in CSharp
    Working with data in CSharp

---
+++ IRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRepositoryUnitOfWork
+{
+    /// <summary>
+    /// Allow work with entities (No transaction)
+    /// </summary>
+    public interface IRepository : IDisposable
+    {
+        /// <summary>
+        /// Create a new Entity for Insert in the database
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <param name="newEntity"></param>
+        /// <returns>The entity created</returns>
+        TEntity Create<TEntity>(TEntity newEntity) where TEntity: class;
+
+        /// <summary>
+        /// Update a entity for update in the database
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <param name="updateEntity"></param>
+        /// <returns>A bool value true if could update else false</returns>
+        bool Update<TEntity>(TEntity updateEntity) where TEntity : class;
+
+        /// <summary>
+        /// Delete a entity
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <param name="deleteEntity"></param>
+        /// <returns>A bool value true if could delete else false</returns>
+        bool Delete<TEntity>(TEntity deleteEntity) where TEntity : class;
+
+        /// <summary>
+        /// Get a entity if exist 
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <param name="criteriaLambda"></param>
+        /// <returns>A entity if exist</returns>
+        TEntity FindEntity<TEntity>(Expression<Func<TEntity, bool>> criteriaLambda) where TEntity : class;
+
+        /// <summary>
+        /// Get a IEnumerable of entities
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <param name="criteriaLambda"></param>
+        /// <returns>List of entities</returns>
+        IEnumerable<TEntity> FindEntities<TEntity>(Expression<Func<TEntity, bool>> criteriaLambda) where TEntity : class;
+    }
+}
---
+++ IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IRepositoryUnitOfWork
+{
+    /// <summary>
+    /// Allow work with entities (Transactionally)
+    /// </summary>
+    public interface IUnitOfWork : IRepository, IDisposable
+    {
+        /// <summary>
+        /// Allow send the transactions to the database
+        /// </summary>
+        /// <returns>Number of transactions do it</returns>
+        int Save();
+    }
+}
