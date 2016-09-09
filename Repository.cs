9175886a2040f3a340abe6ce9e8d8ab5f7af2862
Elias Coelho <elias_coelho19@hotmail.com>
2016-09-09 02:06:54 -0400

    Add files via upload

--- Repository.cs
+++ Repository.cs
@@ -1,120 +1,19 @@
-﻿using IRepositoryUnitOfWork;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Linq.Expressions;
 using System.Data.Entity;
 
-namespace CRepositoryUnitOfWork
+namespace Model
 {
-    public class Repository : IRepository, IDisposable
+    public class Repository : CRepositoryUnitOfWork.Repository, IRepositoryUnitOfWork.IRepository, IDisposable
     {
-        //Context of EntityFrameWork
-        protected DbContext Context;
 
-        //Cosntructor for Repository
-        public Repository(DbContext Context, bool autoDetectChangesEnabled = false, bool proxyCreationEnabled = false)
+        public Repository(bool autoDetectChanges = false, bool proxyCreationEnabled = false)
+            : base(new SchoolDBEntities(), autoDetectChanges, proxyCreationEnabled)
         {
-            this.Context = Context;
-            this.Context.Configuration.AutoDetectChangesEnabled = autoDetectChangesEnabled;
-            this.Context.Configuration.ProxyCreationEnabled = proxyCreationEnabled;
-        }
 
-        //Allow be override and return a int number
-        protected virtual int TrySaveChanges()
-        {
-            return Context.SaveChanges();
-        }
-
-        public TEntity Create<TEntity>(TEntity newEntity) where TEntity : class
-        {
-            TEntity Result = null;
-            try
-            {
-                Result = Context.Set<TEntity>().Add(newEntity);
-                TrySaveChanges();
-            }
-            catch (Exception e)
-            {
-                throw (e);
-            }
-            return Result;
-        }
-
-        public bool Delete<TEntity>(TEntity deleteEntity) where TEntity : class
-        {
-            bool Result = false;
-            try
-            {
-                //Attach to the context (Los adjunta al contexto, pero aun no estan marcados para borrar)
-                Context.Set<TEntity>().Attach(deleteEntity);
-                //Marks the entity to be deleted
-                Context.Set<TEntity>().Remove(deleteEntity);
-                //True if TrySaveChanges() return a number > 0
-                Result = TrySaveChanges() > 0;
-            }
-            catch (Exception e)
-            {
-                throw (e);
-            }
-            return Result;
-        }
-
-        public IEnumerable<TEntity> FindEntities<TEntity>(Expression<Func<TEntity, bool>> criteriaLambda) where TEntity : class
-        {
-            List<TEntity> Result = null;
-            try
-            {
-                Result = Context.Set<TEntity>().Where<TEntity>(criteriaLambda).ToList<TEntity>();
-            }
-            catch (Exception e)
-            {
-                throw (e);
-            }
-            return Result;
-        }
-
-        public TEntity FindEntity<TEntity>(Expression<Func<TEntity, bool>> criteriaLambda) where TEntity : class
-        {
-            TEntity Result = null;
-            try
-            {
-                Result = Context.Set<TEntity>().FirstOrDefault<TEntity>(criteriaLambda);
-            }
-            catch (Exception e)
-            {
-                throw (e);
-            }
-            return Result;
-        }
-
-        public bool Update<TEntity>(TEntity updateEntity) where TEntity : class
-        {
-            bool Result = false;
-            try
-            {
-                //Attach to the context (Los adjunta al contexto, pero aun no estan marcados para modificar)
-                Context.Set<TEntity>().Attach(updateEntity);
-                //Entry to the state of the entity to Mofified state
-                Context.Entry<TEntity>(updateEntity).State = EntityState.Modified;
-                //True if TrySaveChanges() return a number > 0
-                Result = TrySaveChanges() > 0;
-            }
-            catch (Exception e)
-            {
-                throw (e);
-            }
-            return Result;
-        }
-
-        public void Dispose()
-        {
-            if (Context != null)
-            {
-                Context.Dispose();
-            }
         }
     }
 }
--- UnitOfWork.cs
+++ UnitOfWork.cs
@@ -1,40 +1,17 @@
-﻿using IRepositoryUnitOfWork;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Linq.Expressions;
-using System.Data.Entity;
 
-namespace CRepositoryUnitOfWork
+namespace Model
 {
-    public class UnitOfWork : Repository, IRepository, IUnitOfWork, IDisposable
+    public class UnitOfWork : CRepositoryUnitOfWork.Repository, IRepositoryUnitOfWork.IRepository, IDisposable
     {
-        //Constructor
-        public UnitOfWork(DbContext Context, bool autoDetectChangesEnabled = false, bool proxyCreationEnabled = false)
-            :base(Context, autoDetectChangesEnabled, proxyCreationEnabled)
+        public UnitOfWork(bool autoDetectChanges = false, bool proxyCreationEnabled = false)
+            : base(new SchoolDBEntities(), autoDetectChanges, proxyCreationEnabled)
         {
-        }
 
-        int IUnitOfWork.Save()
-        {
-            int Result = 0;
-            try
-            {
-                Result = Context.SaveChanges();
-            }
-            catch (Exception e)
-            {
-                throw (e);
-            }
-            return Result;
         }
-
-        protected override int TrySaveChanges()
-        {
-            return 0;
-        }
-
     }
 }
