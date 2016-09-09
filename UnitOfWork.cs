b6f557d6d38c8da69f889d8273afb92729bb369c
Elias Coelho <elias_coelho19@hotmail.com>
2016-09-09 02:08:00 -0400

    Add files via upload

--- UnitOfWork.cs
+++ UnitOfWork.cs
@@ -1,17 +1,40 @@
-﻿using System;
+﻿using IRepositoryUnitOfWork;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq.Expressions;
+using System.Data.Entity;
 
-namespace Model
+namespace CRepositoryUnitOfWork
 {
-    public class UnitOfWork : CRepositoryUnitOfWork.Repository, IRepositoryUnitOfWork.IRepository, IDisposable
+    public class UnitOfWork : Repository, IRepository, IUnitOfWork, IDisposable
     {
-        public UnitOfWork(bool autoDetectChanges = false, bool proxyCreationEnabled = false)
-            : base(new SchoolDBEntities(), autoDetectChanges, proxyCreationEnabled)
+        //Constructor
+        public UnitOfWork(DbContext Context, bool autoDetectChangesEnabled = false, bool proxyCreationEnabled = false)
+            :base(Context, autoDetectChangesEnabled, proxyCreationEnabled)
         {
-
         }
+
+        int IUnitOfWork.Save()
+        {
+            int Result = 0;
+            try
+            {
+                Result = Context.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                throw (e);
+            }
+            return Result;
+        }
+
+        protected override int TrySaveChanges()
+        {
+            return 0;
+        }
+
     }
 }
